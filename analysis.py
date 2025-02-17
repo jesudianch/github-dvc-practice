f2ba2d1e13aebb6871bc281dffda716b97967043
jesudianch <jchallapalli7@gmail.com>
2025-02-17 16:46:13 -0500

    modify python file

---
+++ analysis.py
@@ -0,0 +1,14 @@
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+print(farewell(user_name))
+
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
