9fb191aaca850109eaff11d23cc7ef8c232b9e9e
jesudianch <jchallapalli7@gmail.com>
2025-02-17 17:06:34 -0500

    added info to the file

--- analysis.py
+++ analysis.py
@@ -1,13 +1,11 @@
 
-def greet(name):
-    return f"Hello, {name}!"
-
+def greet(user_name):
+    return f"Hello, {user_name}!"
 
 def farewell(name):
-    return f"Goodbye, {name}!"
-
+    return f"Goodbye, {user_name}!"
+user_name = "Jesudian"
 print(farewell(user_name))
-
 
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
